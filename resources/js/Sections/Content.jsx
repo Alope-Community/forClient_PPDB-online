275da60cb00f8950c1c4abd17c5ca600dad2ba16
MrFatra <ramadhanfatra2004@gmail.com>
2025-03-19 11:10:14 +0700

    Homepage update

--- resources/js/Sections/Content.jsx
+++ resources/js/Sections/Content.jsx
@@ -1,15 +1,16 @@
+import { usePage } from "@inertiajs/react";
 import ButtonComponent from "../Components/Button";
 
 export default function Content() {
+    const { info } = usePage().props;
+
     return (
         <div className="container mx-auto px-4 md:px-6 lg:px-4 py-16">
-            <div className="flex flex-col-reverse md:flex-row items-center gap-8 ">
+            <div className="flex flex-col-reverse md:flex-row items-center gap-8">
                 <div className="md:w-1/2 text-center md:text-left mt-4 md:mt-0">
                     <h2 className="text-3xl font-bold">MTs LA TAHZAN</h2>
                     <p className="mt-4 text-primary">
-                        Selamat datang di MTs LA Tahzan, madrasah tsanawiyah
-                        berakreditasi A yang berkomitmen mencetak generasi
-                        islami yang unggul dalam akademik.
+                        {info.deskripsi_sekolah || "Deskripsi sekolah belum tersedia."}
                     </p>
                     <div className="mt-8">
                         <ButtonComponent
@@ -22,55 +23,88 @@ export default function Content() {
                 </div>
                 <div className="md:w-1/2 md:flex justify-center hidden">
                     <img
-                        src="../image/logo.png"
+                        src="/image/logo.png"
                         alt="Logo MTs LA Tahzan"
                         className="w-48 md:w-[300px]"
                     />
                 </div>
             </div>
+            <div className="mt-16 bg-secondary text-white py-12 text-center rounded-lg shadow-lg">
+                <p className="text-xl italic font-semibold">
+                    "{info.motto_sekolah || 'Motto belum tersedia.'}"
+                </p>
+            </div>
             <div className="mt-16 text-center">
                 <h2 className="text-3xl font-bold">Program Unggulan</h2>
             </div>
             <div className="mt-8 grid gap-8">
-                <div className="flex flex-col md:flex-row items-center gap-6">
-                    <img
-                        src="../image/tahfidz.jpg"
-                        alt="Tahfidz Quran"
-                        className="w-full md:w-1/2 h-60 object-cover rounded-md"
-                    />
-                    <div className="md:w-1/2">
-                        <h3 className="text-xl font-semibold">Tahfidz Quran</h3>
-                        <p className="mt-2 text-primary">
-                            Program Tahfidz Al-Qur'an di MTs LA Tahzan
-                            membimbing siswa menghafal dan memahami Al-Qur'an
-                            agar mendapatkan keberkahan hidup dunia & akhirat.
+                {info.program_keunggulan &&
+                    JSON.parse(info.program_keunggulan).map((program, index) => (
+                        <div
+                            key={index}
+                            className={`flex flex-col md:flex-row ${index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
+                                } items-center gap-6`}
+                        >
+                            <img
+                                src={`/image/${program.image}`}
+                                alt={program.title}
+                                className="w-full md:w-1/2 h-60 object-cover rounded-md"
+                            />
+                            <div className="md:w-1/2">
+                                <h3 className="text-xl font-semibold">{program.title}</h3>
+                                <p className="mt-2 text-primary">{program.desc}</p>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+
+            <div className="mt-16">
+                <div className="text-center">
+                    <h2 className="text-3xl font-bold">Visi & Misi</h2>
+                </div>
+                <div className="mt-8 grid md:grid-cols-2 gap-8">
+                    <div className="bg-white shadow-lg rounded-lg p-6 border-l-4 border-secondary">
+                        <h3 className="text-xl font-semibold text-secondary">Visi</h3>
+                        <p className="mt-4 mx-5 text-gray-700">
+                            <ul className="mt-4 list-disc text-gray-700">
+                                {info.visi ? JSON.parse(info.visi).map((visi, index) => (
+                                    <li key={index} className="my-4">{visi}</li>
+                                )) : <li>Visi belum tersedia.</li>}
+                            </ul>
                         </p>
+                    </div>
+                    <div className="bg-white shadow-lg rounded-lg p-6 border-l-4 border-secondary">
+                        <h3 className="text-xl font-semibold text-secondary">Misi</h3>
+                        <ul className="mt-4 mx-5 list-disc text-gray-700">
+                            {info.misi ? JSON.parse(info.misi).map((misi, index) => (
+                                <li key={index} className="my-4">{misi}</li>
+                            )) : <li>Misi belum tersedia.</li>}
+                        </ul>
                     </div>
                 </div>
-                <div className="flex flex-col md:flex-row-reverse items-center gap-6">
-                    <img
-                        src="../image/hero/hero.jpg"
-                        alt="Kurikulum Terbaru"
-                        className="w-full md:w-1/2 h-60 object-cover rounded-md"
-                    />
-                    <div className="md:w-1/2">
-                        <h3 className="text-xl font-semibold">
-                            Kurikulum Terbaru
-                        </h3>
-                        <p className="mt-2 text-primary">
-                            MTs LA Tahzan menerapkan kurikulum terbaru dengan
-                            mengintegrasikan sains, seni, dan teknologi untuk
-                            mencetak generasi cerdas dan berakhlak.
-                        </p>
-                    </div>
+                <div className="mt-16 text-center">
+                    <h2 className="text-3xl font-bold">Ekstrakurikuler</h2>
+                </div>
+                <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 gap-8">
+                    {info.eskul ? JSON.parse(info.eskul).map((eskul, index) => (
+                        <div key={index} className="bg-white shadow-md rounded-lg p-6 text-center hover:bg-secondary hover:text-white transition-all duration-300 border-2">
+                            {/* <img
+                                src={`/image/${eskul.image}`}
+                                alt={eskul}
+                                className="w-20 h-20 mx-auto rounded-full object-cover"
+                            /> */}
+                            <h3 className="text-lg font-semibold">{eskul}</h3>
+                        </div>
+                    )) : <p className="text-gray-600">Data ekstrakurikuler belum tersedia.</p>}
                 </div>
             </div>
+
+
             <div className="mt-16 text-center">
                 <h2 className="text-3xl font-bold">Mari Bergabung!!</h2>
                 <p className="mt-2 text-primary">
-                    MTs La Tahzan membuka dua jalur pendaftaran, yaitu Reguler
-                    untuk umum dan Afirmasi bagi siswa berprestasi atau kurang
-                    mampu.
+                    MTs La Tahzan membuka dua jalur pendaftaran, yaitu Reguler untuk umum
+                    dan Afirmasi bagi siswa berprestasi atau kurang mampu.
                 </p>
             </div>
             <div className="grid md:grid-cols-[1fr_auto_1fr] gap-x-16 mt-8 items-center">
@@ -85,30 +119,14 @@ export default function Content() {
                         Persyaratan
                     </h4>
                     <ul className="mt-2 list-none text-center md:text-right">
-                        <li className="flex justify-center md:justify-end items-center">
-                            <span className="mr-2">Pas Foto</span>
-                            <span>•</span>
-                        </li>
-                        <li className="flex justify-center md:justify-end items-center">
-                            <span className="mr-2">Kartu Keluarga</span>
-                            <span>•</span>
-                        </li>
-                        <li className="flex justify-center md:justify-end items-center">
-                            <span className="mr-2">AKTE Kelahiran</span>
-                            <span>•</span>
-                        </li>
-                        <li className="flex justify-center md:justify-end items-center">
-                            <span className="mr-2">KIA/KTP Orang Tua</span>
-                            <span>•</span>
-                        </li>
-                        <li className="flex justify-center md:justify-end items-center">
-                            <span className="mr-2">Ijazah SKL</span>
-                            <span>•</span>
-                        </li>
-                        <li className="flex justify-center md:justify-end items-center">
-                            <span className="mr-2">SKHU/Raport Kelas 5</span>
-                            <span>•</span>
-                        </li>
+                        {["Pas Foto", "Kartu Keluarga", "AKTE Kelahiran", "KIA/KTP Orang Tua", "Ijazah SKL", "SKHU/Raport Kelas 5"].map(
+                            (item, index) => (
+                                <li key={index} className="flex justify-center md:justify-end items-center">
+                                    <span className="mr-2">{item}</span>
+                                    <span>•</span>
+                                </li>
+                            )
+                        )}
                     </ul>
                     <div className="mt-5 text-center md:text-right md:mt-10">
                         <ButtonComponent
@@ -116,13 +134,15 @@ export default function Content() {
                             variant="secondary"
                             size="lg"
                             color="white"
-                            link={"/pendaftaran-jalur-reguler"}
+                            link="/pendaftaran-jalur-reguler"
                         />
                     </div>
                 </div>
+
                 <div className="hidden md:flex justify-center">
                     <div className="w-[2px] h-[350px] bg-secondary"></div>
                 </div>
+
                 <div className="pb-4 md:pb-0 md:pl-12 flex flex-col md:items-start items-center">
                     <h3 className="text-xl font-semibold text-center md:text-left">
                         Jalur Afirmasi
@@ -134,13 +154,11 @@ export default function Content() {
                         Persyaratan
                     </h4>
                     <ul className="mt-2 text-primary list-disc list-inside text-center md:text-left">
-                        <li>Pas Foto</li>
-                        <li>Kartu Keluarga</li>
-                        <li>AKTE Kelahiran</li>
-                        <li>KIA/KTP Orang Tua</li>
-                        <li>Ijazah SKL</li>
-                        <li>SKHU/Raport Kelas 5</li>
-                        <li>KIP/PKH/KIP/SKTM</li>
+                        {["Pas Foto", "Kartu Keluarga", "AKTE Kelahiran", "KIA/KTP Orang Tua", "Ijazah SKL", "SKHU/Raport Kelas 5", "KIP/PKH/KIP/SKTM"].map(
+                            (item, index) => (
+                                <li key={index}>{item}</li>
+                            )
+                        )}
                     </ul>
                     <div className="mt-5 text-center md:text-left">
                         <ButtonComponent
@@ -148,7 +166,7 @@ export default function Content() {
                             variant="secondary"
                             size="lg"
                             color="white"
-                            link={"/pendaftaran-jalur-afirmasi"}
+                            link="/pendaftaran-jalur-afirmasi"
                         />
                     </div>
                 </div>
